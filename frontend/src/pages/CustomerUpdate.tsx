4cab3f549b33b9ac8ddf848a3fff1fe66e781ab5
Truong Duy <s3818381@rmit.edu.vn>
2021-06-24 02:54:42 +0700

    feat: 🎉 customer input UI page

--- frontend/src/pages/CustomerUpdate.tsx
+++ frontend/src/pages/CustomerUpdate.tsx
@@ -4,13 +4,17 @@ import {
   FormLabel,
   FormErrorMessage,
   Button,
-  Input
+  Input,
+  Flex,
+  Image,
+  Center
 } from "@chakra-ui/react";
 import {
   Formik,
   Form,
   Field
 } from "formik";
+import customer from "../assets/img/customer.jpg";
 
 interface ResultPageProps {}
 
@@ -74,70 +78,82 @@ export default class CustomerUpdate extends React.Component<ResultPageProps, Res
       <div style={{ paddingTop: 100 }}>
         <div className="container">
           <p className="header">NEW CUSTOMER INPUT</p>
-          <Formik
-            initialValues={{ firstName: '', lastName: '', dob: '' }}
-            onSubmit={(values, actions) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                actions.setSubmitting(false);
-              }, 1000);
-            }}
-          >
-            {(props) => (
-              <Form>
+          <Flex style={{marginTop: 50}}>
+            <Formik
+              initialValues={{ firstName: '', lastName: '', dob: '' }}
+              onSubmit={(values, actions) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  actions.setSubmitting(false);
+                }, 1000);
+              }}
+            >
+              {(props) => (
+                <Form style={{marginLeft: 50  }}>
 
-                {/* First Name */}
-                <Field name="firstName" validate={this.validateFirstName}>
-                  {({ field, form }: { field: string, form: any }) => (
-                    <FormControl isInvalid={form.errors.firstName && form.touched.firstName}>
-                      <FormLabel htmlFor="first-name">First name</FormLabel>
-                      <Input {...field} id="first-name" placeholder="First Name" />
-                      <FormErrorMessage>{form.errors.firstName}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
+                  {/* First Name */}
+                  <Field name="firstName" validate={this.validateFirstName}>
+                    {({ field, form }: { field: string, form: any }) => (
+                      <FormControl isInvalid={form.errors.firstName && form.touched.firstName}>
+                        <FormLabel htmlFor="first-name">First name</FormLabel>
+                        <Input {...field} id="first-name" placeholder="First Name" width="400px"/>
+                        <FormErrorMessage>{form.errors.firstName}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
 
-                {/* Last Name */}
-                <Field name="lastName" validate={this.validateLastName}>
-                  {
-                  ({ field, form }: { field: string, form: any }) => (
-                    <FormControl
-                      isInvalid={form.errors.lastName && form.touched.lastName}
+                  {/* Last Name */}
+                  <Field name="lastName" validate={this.validateLastName}>
+                    {
+                    ({ field, form }: { field: string, form: any }) => (
+                      <FormControl
+                        isInvalid={form.errors.lastName && form.touched.lastName}
+                      >
+                        <FormLabel htmlFor="lastName">Last name</FormLabel>
+                        <Input {...field} id="lastName" placeholder="Last Name" width="400px"/>
+                        <FormErrorMessage>{form.errors.lastName}</FormErrorMessage>
+                      </FormControl>
+                    )
+                    }
+                  </Field>
+
+                  {/* DOB */}
+                  <Field name="dob" validate={this.validateDOB}>
+                    {
+                    ({ field, form }: { field: string, form: any }) => (
+                      <FormControl
+                        isInvalid={form.errors.dob && form.touched.dob}
+                      >
+                        <FormLabel htmlFor="dob">Date of Birth</FormLabel>
+                        <Input {...field} id="dob" placeholder="dd-MM-yyyy" width="400px"/>
+                        <FormErrorMessage>{form.errors.dob}</FormErrorMessage>
+                      </FormControl>
+                    )
+                    }
+                  </Field>
+                  <Center>
+                    <Button
+                      mt={4}
+                      textColor="white"
+                      bg="#37517e"
+                      height="48px"
+                      width="100px"
+                      isLoading={props.isSubmitting}
+                      type="submit"
                     >
-                      <FormLabel htmlFor="lastName">Last name</FormLabel>
-                      <Input {...field} id="lastName" placeholder="Last Name" />
-                      <FormErrorMessage>{form.errors.lastName}</FormErrorMessage>
-                    </FormControl>
-                  )
-                  }
-                </Field>
+                      Submit
+                    </Button>
+                  </Center>
+                </Form>
+              )}
+            </Formik>
+            <div style={{marginLeft: 150}}>
+                <Image height="270px" width="400px" src={customer} alt="Dan Abramov" />
+            </div>
+          </Flex>
 
-                {/* DOB */}
-                <Field name="dob" validate={this.validateDOB}>
-                  {
-                  ({ field, form }: { field: string, form: any }) => (
-                    <FormControl
-                      isInvalid={form.errors.dob && form.touched.dob}
-                    >
-                      <FormLabel htmlFor="dob">Date of Birth</FormLabel>
-                      <Input {...field} id="dob" placeholder="dd-MM-yyyy" />
-                      <FormErrorMessage>{form.errors.dob}</FormErrorMessage>
-                    </FormControl>
-                  )
-                  }
-                </Field>
-
-                <Button
-                  mt={4}
-                  colorScheme="teal"
-                  isLoading={props.isSubmitting}
-                  type="submit"
-                >
-                  Submit
-                </Button>
-              </Form>
-            )}
-          </Formik>
+          {/* Final padding page */}
+          <div style={{ padding: 50 }}></div>
         </div>
       </div>
     );
