927c4f864bfdd933ac6a860979a5bdaf25011955
CallMeDKay <s3812649@rmit.edu.vn>
2021-06-23 21:13:26 +0700

    feat: env configuration and data input

--- model-backend/app.py
+++ model-backend/app.py
@@ -5,16 +5,24 @@ import pandas as pd;
 from flask import Flask, jsonify, request
 import numpy as np
 import ast
+from dotenv import load_dotenv
+import src.constant as constant;
+
+# Load .env
+load_dotenv()
 
 # Initialize Flask
 app = Flask(__name__)
 
+
 # App health check
 @app.route("/healthcheck", methods=["GET"])
 def healthcheck():
-  msg = "Server is running well!"
+  msg = "ModelServer is running well at PORT"
+  print(os.getenv("FLASK_APP"))
+  print(constant.PROD)
   return jsonify({"message": msg})
 
 # Execute Flask App
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host=os.getenv("HOST"),port=os.getenv("PORT"),debug=not constant.PROD)
