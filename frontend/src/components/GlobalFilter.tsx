c835de98ab4cc7e63bb5cfd3fd0d0d63964681ad
thien <s3817852@rmit.edu.vn>
2021-06-30 21:40:18 +0700

    fetch data in database

--- frontend/src/components/GlobalFilter.tsx
+++ frontend/src/components/GlobalFilter.tsx
@@ -1,81 +1,40 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Input, Flex, Heading, filter} from '@chakra-ui/react';
 import '../assets/css/Table.css';
+import {useQuery, gql} from "@apollo/client"
 
-interface Customer{
-    name: string;
-    score: string;
-    date: string;
-    interest_rate: string;
-    months: string;
-    loan_amount: string;
-    id: number;
-}
 
-const defaultProps: Customer[] = [
-    {
-        "name":"Vo Tran Truong Duy",
-        "score":"89",
-        "date":"12/12/2019",
-        "interest_rate":"10",
-        "months":"6",
-        "loan_amount":"15.000.000",
-        "id":1
-    },
-    {
-        "name":"Nguyen Ngoc Dang Hung",
-        "score":"90",
-        "date":"21/9/2019",
-        "interest_rate":"4",
-        "months":"3",
-        "loan_amount":"8.000.000",
-        "id":2
-    },
-    {
-        "name":"Nguyen Pham Quoc Minh",
-        "score":"90",
-        "date":"8/3/2020",
-        "interest_rate":"6",
-        "months":"3",
-        "loan_amount":"5.000.000",
-        "id":3
-    },
-    {
-        "name":"Dat Corn",
-        "score":"90",
-        "date":"7/11/2018",
-        "interest_rate":"5",
-        "months":"6",
-        "loan_amount":"7.000.000",
-        "id":4
-    },
-    {
-        "name":"Thao Tho Ngoc",
-        "score":"9",
-        "date":"18/9/2020",
-        "interest_rate":"7",
-        "months":"12",
-        "loan_amount":"20.000.000",
-        "id":5
-    },
-    {
-        "name":"Vu Duc Duy",
-        "score":"69",
-        "date":"11/4/2020",
-        "interest_rate":"5",
-        "months":"3",
-        "loan_amount":"2.000.000",
-        "id":5
+const CUS_CREDIT_HISTORY_LIST = gql`
+    query CustomerCreditHistoryList {
+        customers{
+            id
+            firstName
+            lastName
+            dateOfBirth
+            creditHistoryList{
+                negScore
+                posScore
+                createdAt
+            }
+        }
     }
-]
+`
 
 const GlobalFilter: React.FC = () => {
-    const [customer, setCustomer]: [Customer[], (posts: Customer[]) => void] = React.useState(defaultProps);
     const [filter, setFilter]: [string, (search: string) => void] = React.useState("");
 
     const filterRequire = (event: {target: {value: string; }; }) =>{
         setFilter(event.target.value)
     } 
+
+    const {loading, data} = useQuery(CUS_CREDIT_HISTORY_LIST)
+    const convertDate = (unixTimestamp: number) => {
+        unixTimestamp /= 1
+        const result = new Date(unixTimestamp)
+        const humanDateFormat = result.toLocaleString() 
+        return humanDateFormat
+    }
+    if(loading) return null;
 
     return (
          <>
@@ -86,34 +45,35 @@ const GlobalFilter: React.FC = () => {
             <i className="fas fa-search" style={{fontSize: "35px" , marginLeft:"10px"}}></i>
         </Flex>
         </Flex>
+        
         <table>
             <tr>
                 <th>Name</th>
-                <th>Score</th>
-                <th>Date</th>
-                <th>Interest Rate</th>
-                <th>Months</th>
-                <th>Loan Amount</th>
+                <th>Negative Score</th>
+                <th>Positive Score</th>
+                <th>Created At</th>
+                <th>Data Of Birth</th>
             </tr>
-
-            {customer.map((customer) => {
-                    if (filter == "" || customer.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())){
-                        return (
-                            <tr>
-                                <td>{customer.name}</td>
-                                <td>{customer.score}</td>
-                                <td>{customer.date}</td>
-                                <td>{customer.interest_rate}</td>
-                                <td>{customer.months}</td>
-                                <td>{customer.loan_amount}</td>
-                                
-                            </tr>
-                        );
-                    }
-                    
-                    return null;
+            {data.customers.map((val:any) => {
+                const { id , lastName, firstName, creditHistoryList , dateOfBirth} = val
+                const score  = creditHistoryList[0]
+                const dateFormated = convertDate(score.createdAt)
+                if (filter == "" || val.firstName.toLocaleLowerCase().includes(filter.toLocaleLowerCase())){
+                    return (
+                        <tr>
+                            <td>{firstName} {lastName}</td>
+                            <td>{score.negScore}</td>
+                            <td>{score.posScore}</td>
+                            <td> {dateFormated}</td>
+                            <td>{dateOfBirth}</td>
+                                    
+                                    
+                        </tr>
+                    )
+                }
                 
-                })}
+            })}
+            
         </table>
         </>
         
