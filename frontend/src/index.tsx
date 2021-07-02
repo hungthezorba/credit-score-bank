ea49dcf2dea60fcda17f816e135f0277f966c844
hungnguyen <hungthezorba@gmail.com>
2021-07-02 09:33:59 +0700

    test: add proxy

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -12,7 +12,7 @@ import {
 } from "@apollo/client";
 
 const link = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://api.minhthings.com/graphql",
   credentials: 'include'
 })
 
